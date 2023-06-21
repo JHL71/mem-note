e5cd46ea2f2e2fe7daec600509c77bfb742307cf
JHL71 <neko0717@naver.com>
2023-06-21 21:31:06 +0900

    server setting

---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Typescript Server");
+});
+
+app.listen(port, () => {
+    
+})
