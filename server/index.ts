ae8cf86046649d70e27a80023fcbf6fde4303344
JHL71 <neko0717@naver.com>
2023-06-22 22:36:29 +0900

    test

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,21 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+// import bodyparser from 'body-parser';
 
 const app: Express = express();
-const port = 3000;
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("Typescript Server");
+    res.send({data: "typescript server"});
 });
+
+app.post('/post', (req: Request, res: Response) => {
+    console.log(req.body);
+    res.send({data: 'check'});
+})
 
 app.listen(port, () => {
     
