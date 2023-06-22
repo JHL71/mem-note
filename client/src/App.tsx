03da4664be7b4aa16bc0e4759f10859d41a96aca
JHL71 <neko0717@naver.com>
2023-06-22 22:37:00 +0900

    client server test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+
+
 
 function App() {
+
+  const url = `http://localhost:3001/`;
+
+  const sendD = () => {
+    fetch(`${url}post`, {
+      method: 'post', 
+      mode: 'cors',
+      credentials: 'same-origin',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ data: 'hello'})
+    })
+    .then((re) => re.json())
+    .then((res: any) => console.log(res.data))
+  }
+
+  useEffect(() => {
+    fetch(url)
+    .then((re) => re.json())
+    .then((res) => {
+      console.log(res.data);
+    });
+  })
+
   return (
-    <div>
-      hello world;
+    <div onClick={() => sendD()}>
+      hello world
     </div>
   );
 }
