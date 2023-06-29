fea08accdb4b2569fb6620fb0a74d9df4ad136b8
JHL71 <neko0717@naver.com>
2023-06-29 11:26:30 +0900

    modify memo type

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,17 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 
-const mem = [{title: 'dummy', text: 'dummy'}, {title: 'test', text: 'testdummy'}];
+interface memI {
+    title: string,
+    text: string
+}
+
+const mem: { [key: number]: memI  } = {
+    1 : {title: 'dummy', text: 'dummy'},
+    2 : {title: 'test', text: 'testdummy'}
+};
+
+let top = 3;
 
 
 app.get('/', (req: Request, res: Response) => {
@@ -18,9 +28,15 @@ app.get('/', (req: Request, res: Response) => {
 
 app.post('/post', (req: Request, res: Response) => {
     const [title, text] = [req.body.title, req.body.text];
-    mem.push({title, text});
+    mem[top] = {title, text};
+    top++;
     console.log(mem);
     res.send({data: 'check'});
+})
+
+app.delete('/delete', (req: Request, res: Response) => {
+    const num = req.body.num;
+    delete mem[num];
 })
 
 app.listen(port, () => {
